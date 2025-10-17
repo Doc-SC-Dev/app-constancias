0821c32934cbd9de0af1c11ab6ff99e4f1576fc6
Tomas Bravo <tomas.b.c@outlook.com>
2025-10-17 18:46:01 -0300

    init

---
+++ src/lib/db.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export const db = new PrismaClient().$extends(withAccelerate());
