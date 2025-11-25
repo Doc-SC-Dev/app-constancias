ea3ea1b96083d3c025635fb8b963d64a437b7f46
Tomas Bravo <tomas.b.c@outlook.com>
2025-11-25 14:41:08 -0300

    feat: add certificate selection to requests

--- src/app/(dashboard)/action.ts
+++ src/app/(dashboard)/action.ts
@@ -3,6 +3,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
+import { db } from "@/lib/db";
 
 export const logoutAction = async () => {
   console.log("on logout");
@@ -10,4 +11,13 @@ export const logoutAction = async () => {
     headers: await headers(),
   });
   redirect("/login");
+};
+
+export const getRequestsTypes = async () => {
+  return await db.certificate.findMany({
+    select: {
+      name: true,
+      id: true,
+    },
+  });
 };
