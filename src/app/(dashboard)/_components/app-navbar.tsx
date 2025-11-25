d8fc40cda5553e1068fd42f1ea08f4358c524536
Tomas Bravo <tomas.b.c@outlook.com>
2025-11-25 14:55:03 -0300

    refactor: simplify app navbar component structure

--- src/app/(dashboard)/_components/app-navbar.tsx
+++ src/app/(dashboard)/_components/app-navbar.tsx
@@ -2,12 +2,12 @@
 
 import Image from "next/image";
 import { useIsMobile } from "@/hooks/use-mobile";
-import type { User } from "@/lib/types/users";
+import type { UserWithRut } from "@/lib/types/users";
 import { AppAvatar } from "./app-avatar";
 import { AppSideBarTrigger } from "./app-sidebar-trigger";
 
 type NavBarProps = {
-  user: User;
+  user: UserWithRut;
 };
 
 export function AppNavBar({ user }: NavBarProps) {
