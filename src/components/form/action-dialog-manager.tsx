8b1c4990a268ebf1c8c3eb5e973e6d786e5953bb
Tomas Bravo <tomas.b.c@outlook.com>
2025-11-25 15:12:41 -0300

    feat(components): add icon and variant props to ActionDialogManager

--- src/components/form/action-dialog-manager.tsx
+++ src/components/form/action-dialog-manager.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Dialog } from "@radix-ui/react-dialog";
-import { Plus } from "lucide-react";
-import { useState } from "react";
+import { type LucideProps, Plus } from "lucide-react";
+import {
+  type ForwardRefExoticComponent,
+  type RefAttributes,
+  useState,
+} from "react";
 import { Button } from "@/components/ui/button";
 import { type Action, Actions } from "@/lib/types/action";
 import ActionButton from "./action-button";
@@ -14,15 +18,21 @@ type ActionDialogProps<T> = {
   editDialog?: React.ComponentType<{ data: T; closeDialog: () => void }>;
   deleteDialog?: React.ComponentType<{ data: T; closeDialog: () => void }>;
   createDialog?: React.ComponentType<{ closeDialog: () => void }>;
+  icon?: ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+  >;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost";
 };
 
 export default function ActionDialogManager<T>({
   data,
-  triggerLabel = "Agregar",
+  triggerLabel = "",
   viewDialog: ViewDialog,
   editDialog: EditDialog,
   deleteDialog: DeleteDialog,
   createDialog: CreateDialog,
+  icon: Icon = Plus,
+  variant = "default",
 }: ActionDialogProps<T>) {
   const [showDialog, setShowDialog] = useState<boolean>(false);
   const [action, setAction] = useState<Action>(Actions.VIEW);
@@ -61,9 +71,9 @@ export default function ActionDialogManager<T>({
           }
         />
       ) : (
-        <Button variant="default" onClick={() => setShowDialog(true)}>
+        <Button variant={variant} onClick={() => setShowDialog(true)}>
           {triggerLabel}
-          <Plus />
+          <Icon />
         </Button>
       )}
       <Dialog open={showDialog} onOpenChange={setShowDialog}>
