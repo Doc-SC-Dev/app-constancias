9123d7e62351206ba6a1f6c8ecbe1cbded67d06e
Tomas Bravo <tomas.b.c@outlook.com>
2025-11-25 15:01:41 -0300

    refactor: improve dashboard card layout and styling

--- src/app/(dashboard)/dashboard/_components/dashboard-card.tsx
+++ src/app/(dashboard)/dashboard/_components/dashboard-card.tsx
@@ -1,10 +1,19 @@
+"use client";
+import { Plus } from "lucide-react";
 import Link from "next/link";
+import type { ReactNode } from "react";
+import ActionDialogManager from "@/components/form/action-dialog-manager";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { menus } from "@/lib/types/menus";
+import NewUserDialog from "../users/_components/newuser-dialog";
+import CreateRequestDialog from "./create-request-dialog";
 
 interface DashboardCardProps {
   title: string;
@@ -14,16 +23,40 @@ interface DashboardCardProps {
 }
 
 export function DashboardCard({ title, description, url }: DashboardCardProps) {
+  const selectDialog: () => ReactNode = () => {
+    switch (title) {
+      case menus.students.name:
+        return <></>;
+      case menus.history.name:
+        return <CreateRequestDialog />;
+      case menus.activities.name:
+        return <></>;
+      case menus.users.name:
+        return <NewUserDialog />;
+      default:
+        return <></>;
+    }
+  };
   return (
-    <Card className=" h-30 hover:bg-accent hover:text-accent-foreground min-w-2xs max-w-xl">
-      <Link href={url} className="place-self-stretch">
-        <CardHeader>
-          <CardTitle>{title}</CardTitle>
-          <CardDescription className="on-hover:text-accent-foreground/10">
-            {description}
-          </CardDescription>
-        </CardHeader>
-      </Link>
+    <Card className=" max-h-sm min-w-2xs max-w-xl">
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between">
+          {title}
+          <Button variant="link" asChild>
+            <Link href={url}>Ir</Link>
+          </Button>
+        </CardTitle>
+        <CardDescription className="on-hover:text-accent-foreground/10">
+          {description}
+        </CardDescription>
+        <CardFooter className="flex items-center justify-center">
+          <ActionDialogManager
+            createDialog={selectDialog}
+            triggerLabel={`Crear ${title}`}
+            variant="ghost"
+          />
+        </CardFooter>
+      </CardHeader>
     </Card>
   );
 }
