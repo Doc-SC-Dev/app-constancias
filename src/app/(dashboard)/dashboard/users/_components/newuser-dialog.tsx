857f702e37f280458bdcf3e172545a4193987c93
Tomas Bravo <tomas.b.c@outlook.com>
2025-12-15 21:11:48 -0300

    feat(users): add admission date and gender fields to user creation
    Add calendar picker for admission date and map gender selection to genre field in user creation action.

--- src/app/(dashboard)/dashboard/users/_components/newuser-dialog.tsx
+++ src/app/(dashboard)/dashboard/users/_components/newuser-dialog.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { arktypeResolver } from "@hookform/resolvers/arktype";
-import { useForm } from "react-hook-form";
+import { CalendarIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { FormInput } from "@/components/form/FormInput";
 import { FormSelect } from "@/components/form/FormSelect";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   DialogClose,
   DialogContent,
@@ -14,7 +16,24 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { FieldGroup, FieldSeparator } from "@/components/ui/field";
+import {
+  Field,
+  FieldContent,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { SelectItem } from "@/components/ui/select";
 import { Spinner } from "@/components/ui/spinner";
 import { AcademicGrade, Genre } from "@/generated/prisma";
@@ -62,94 +81,147 @@ export default function NewUserDialog({ closeDialog }: DialogContentProps) {
     }
   };
   return (
-    <DialogContent>
+    <DialogContent className="max-w-xl w-lg">
       <DialogHeader className="mb-4">
         <DialogTitle>Crear nuevo usuario</DialogTitle>
         <DialogDescription>
           Ingresar datos para crear un nuevo usuario
         </DialogDescription>
       </DialogHeader>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FieldGroup className="gap-4">
-          <FormInput
-            label="Nombre"
-            control={control}
-            name="name"
-            description="Ingresar el nombre completo del nuevo usuario"
-          />
-          <FormInput
-            label="Email"
-            control={control}
-            name="email"
-            description="Ingresar el correo que tendrá asociado la cuenta del nuevo usuario"
-          />
-          <FormInput
-            label="Rut"
-            control={control}
-            name="rut"
-            description="La contraseña del nuevo usuario será su RUT sin puntos y con guión"
-          />
+      <div className="overflow-y-scroll max-h-96 overflow-x-hidden">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-4 w-full">
+            <FormInput
+              label="Nombre"
+              control={control}
+              name="name"
+              description="Ingresar el nombre completo del nuevo usuario"
+            />
+            <FormInput
+              label="Email"
+              control={control}
+              name="email"
+              description="Ingresar el correo que tendrá asociado la cuenta del nuevo usuario"
+            />
+            <FormInput
+              label="Rut"
+              control={control}
+              name="rut"
+              description="La contraseña del nuevo usuario será su RUT sin puntos y con guión"
+            />
 
-          <FormSelect
-            label="Grado académico"
-            control={control}
-            name="academicGrade"
-            description="Seleccione el grado académico del nuevo usuario"
-          >
-            {Object.values(AcademicGrade).map((grade) => (
-              <SelectItem value={grade} key={grade}>
-                {grade.toLowerCase()}
-              </SelectItem>
-            ))}
-          </FormSelect>
-        </FieldGroup>
-        <FieldGroup>
-          <FormSelect
-            label="Rol"
-            control={control}
-            name="role"
-            description="Seleccione el rol que tendrá en la plataforma el nuevo usuario"
-          >
-            {[...Object.values(Roles)].map((rol) => {
-              if (
-                data?.user.role !== Roles.SUPERADMIN &&
-                rol === Roles.SUPERADMIN
-              ) {
-                return undefined;
-              }
-              return (
-                <SelectItem value={rol} key={rol}>
-                  {rol}
+            <FormSelect
+              label="Grado académico"
+              control={control}
+              name="academicGrade"
+              description="Seleccione el grado académico del nuevo usuario"
+            >
+              {Object.values(AcademicGrade).map((grade) => (
+                <SelectItem value={grade} key={grade}>
+                  {grade.toLowerCase()}
                 </SelectItem>
-              );
-            })}
-          </FormSelect>
-          {role === "student" && (
-            <>
-              <FieldSeparator />
-              <FormInput
-                label="Matrícula"
-                control={control}
-                name="studentId"
-                description="Ingresar el número de matrícula del nuevo estudiante"
-              />
-            </>
-          )}
-        </FieldGroup>
-        <DialogFooter className="mt-6">
-          <DialogClose asChild onClick={() => reset()}>
-            <Button variant="outline">Cancelar</Button>
-          </DialogClose>
-          <Button
-            type="submit"
-            variant="default"
-            disabled={formState.isSubmitting}
-          >
-            {formState.isSubmitting && <Spinner />}
-            Crear
-          </Button>
-        </DialogFooter>
-      </form>
+              ))}
+            </FormSelect>
+            <FormSelect
+              name="gender"
+              control={control}
+              label="Género"
+              description="Seleccione el género del nuevo usuario"
+            >
+              {Object.values(Genre).map((gender) => (
+                <SelectItem value={gender} key={gender}>
+                  {gender.toLowerCase()}
+                </SelectItem>
+              ))}
+            </FormSelect>
+
+            <FormSelect
+              label="Rol"
+              control={control}
+              name="role"
+              description="Seleccione el rol que tendrá en la plataforma el nuevo usuario"
+            >
+              {[...Object.values(Roles)].map((rol) => {
+                if (
+                  data?.user.role !== Roles.SUPERADMIN &&
+                  rol === Roles.SUPERADMIN
+                ) {
+                  return undefined;
+                }
+                return (
+                  <SelectItem value={rol} key={rol}>
+                    {rol}
+                  </SelectItem>
+                );
+              })}
+            </FormSelect>
+
+            {role === "student" && (
+              <>
+                <FieldSeparator />
+                <FormInput
+                  label="Matrícula"
+                  control={control}
+                  name="studentId"
+                  description="Ingresar el número de matrícula del nuevo estudiante"
+                />
+
+                <Controller
+                  name="admissionDate"
+                  control={control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldContent>
+                        <FieldLabel>Fecha de Admisión</FieldLabel>
+                        <InputGroup>
+                          <InputGroupInput
+                            {...field}
+                            aria-invalid={fieldState.invalid}
+                            value={field.value?.toLocaleDateString("es-CL")}
+                          />
+                          <InputGroupAddon align="inline-end">
+                            <Popover>
+                              <PopoverTrigger asChild>
+                                <Button type="button" variant="ghost">
+                                  <CalendarIcon />
+                                </Button>
+                              </PopoverTrigger>
+                              <PopoverContent>
+                                <Calendar
+                                  selected={field.value}
+                                  onSelect={field.onChange}
+                                  mode="single"
+                                  captionLayout="dropdown"
+                                />
+                              </PopoverContent>
+                            </Popover>
+                          </InputGroupAddon>
+                        </InputGroup>
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </FieldContent>
+                    </Field>
+                  )}
+                />
+              </>
+            )}
+          </FieldGroup>
+          <DialogFooter className="mt-6">
+            <DialogClose asChild onClick={() => reset()}>
+              <Button variant="outline">Cancelar</Button>
+            </DialogClose>
+            <Button
+              type="submit"
+              variant="default"
+              disabled={formState.isSubmitting}
+            >
+              {formState.isSubmitting && <Spinner />}
+              Crear
+            </Button>
+          </DialogFooter>
+        </form>
+      </div>
     </DialogContent>
   );
 }
--- src/app/(dashboard)/dashboard/users/actions.ts
+++ src/app/(dashboard)/dashboard/users/actions.ts
@@ -51,6 +51,7 @@ export async function createUser(userData: UserCreate) {
           ...newUserData,
           password,
           data: {
+            genre: newUserData.gender,
             rut,
             academicGrade: academicGrade ?? "",
           },
