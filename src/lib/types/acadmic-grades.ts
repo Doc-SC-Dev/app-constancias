c9fcd85eb35a036637ad0f0e84ff57f552bbdc9b
Tomas Bravo <tomas.b.c@outlook.com>
2026-03-17 10:09:09 -0300

    refactor(shared): update link button and grade schemas
    Update shared components and type definitions.
    
    - Updated src/components/link-action-button.tsx
    - Updated src/lib/types/acadmic-grades.ts

--- src/lib/types/acadmic-grades.ts
+++ src/lib/types/acadmic-grades.ts
@@ -21,3 +21,28 @@ export const AcademicDegreeCreateSchema = type({
 });
 
 export type AcademicDegreeCreateDto = typeof AcademicDegreeCreateSchema.infer;
+
+export const AcademicDegreeEditSchema = type({
+  id: type("string"),
+  name: type("string")
+    .moreThanLength(1)
+    .configure({
+      message: () => "El nombre es requerido",
+    }),
+  abbrevFem: type("string")
+    .moreThanLength(1)
+    .configure({
+      message: () => "La abreviatura femenina es requerida",
+    })
+    .pipe((val) => (val.at(-1) === "." ? val : `${val}.`)),
+  abbrevMas: type("string")
+    .moreThanLength(1)
+    .configure({
+      message: () => "La abreviatura masculina es requerida",
+    })
+    .pipe((val) => (val.at(-1) === "." ? val : `${val}.`)),
+  abbrevFemId: type("string"),
+  abbrevMasId: type("string"),
+});
+
+export type AcademicDegreeEditDto = typeof AcademicDegreeEditSchema.infer;
